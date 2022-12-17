7417996b49ba3eef76562b65593b2e27f6ad9723
Rafael Marques <--global>
2022-12-17 12:28:00 -0300

    alterações

--- js/Search.js
+++ js/Search.js
@@ -75,7 +75,6 @@ export class searchPokemon extends Search {
     function checkChar(e) {
       var char = String.fromCharCode(e.keyCode)
 
-      console.log(char)
       var pattern = "[a-zA-Z0-9]"
       if (char.match(pattern)) {
         return true
